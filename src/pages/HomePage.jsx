06af28ddb486ff91ba29d6f994e14b5e68146581
Kirill Gudron <dima.fasmin446@mail.ru>
2023-01-24 11:09:39 +0300

    Add new styles,add format functions and item component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,57 @@
+// Income - доходы
+// Expense - расходы
+
+import Item from "../components/Item/Item";
+
+import { useState } from "react";
+import formatMoney from "../components/Utils/formatMoney";
+
+const TYPES = {
+    INCOME: 'income',
+    EXPENSE: 'expense'
+}
+
+const initialItemsState = [
+    {
+        category: 'Продукты',
+        total: 2000,
+        type: TYPES.EXPENSE,
+        id: 1,
+        created_at: new Date('2022-01-01'),
+    },
+    {
+        category: 'Зарплата',
+        total: 50000,
+        type: TYPES.INCOME,
+        id: 2,
+        created_at: new Date('2023-01-01'),
+    },
+    {
+        category: 'Автомобиль',
+        total: 16990,
+        type: TYPES.EXPENSE,
+        id: 3,
+        created_at: new Date('2023-03-03'),
+    }
+];
+
 const HomePage = () => {
+    const [items, setItems] = useState(initialItemsState);
+
     return (
         <section>
             <div className="container">
                 <div className="layout">
-
                     <header>
-                        <h1>Общий баланс: 30 000 руб.</h1>
+                        <h1>Общий баланс: {formatMoney(70000)}</h1>
                     </header>
 
                     <div className="form">
                         <h2>Добавить операцию</h2>
-                        <form action="">
-                            <input type="text" name="total" placeholder="30 000 руб." />
+                        <form>
+                            <input type="text" name="total" placeholder="30 000 руб" />
                             <select name="category">
-                                <option value="salary">Заработая плата</option>
+                                <option value="salary">Заработная плата</option>
                             </select>
                             <button className="button">
                                 Добавить операцию
@@ -25,59 +63,37 @@ const HomePage = () => {
                         <h2>Операции</h2>
 
                         <div className="filter">
-                            <button className="filter__butoon">Все операции</button>
-                            <button className="filter__butoon">Все доходы</button>
-                            <button className="filter__butoon">Все расходы</button>
+                            <button className="filter_button">Все операции</button>
+                            <button className="filter_button">Все доходы</button>
+                            <button className="filter_button">Все расходы</button>
                         </div>
 
                         <div className="items">
-                            <div className="item">
-                                <div className="item_left">
-                                    <div className="circle expense">
-                                        {/* Expense */}
-                                        <i class="fa-solid fa-basket-shopping"></i>
-                                    </div>
 
-                                    <p className="category">
-                                        Категория: продукты
-                                    </p>
-                                </div>
-                                <div className="item__right">
-                                    <p className="total">
-                                        2 2500 руб.
-                                    </p>
-
-                                    <button className="item__button">Remove</button>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="items">
-                            <div className="item">
-                                <div className="item_left">
-                                    <div className="circle income">
-
-                                        {/* Income */}
-                                        <i class="fa-solid fa-money-check-dollar"></i>
-                                    </div>
-
-                                    <p className="category">
-                                        Категория: Заробная плата
-                                    </p>
-                                </div>
-                                <div className="item__right">
-                                    <p className="total">
-                                        2 2500 руб.
-                                    </p>
-
-                                    <button className="item__button">Remove</button>
-                                </div>
-                            </div>
+                            {
+                                items.map((item) => <Item key={item.id} item={item} />)
+                            }
                         </div>
 
                         <div className="pagination">
                             <button className="pagination__button">
                                 <span>1</span>
+                            </button>
+
+                            <button className="pagination__button">
+                                <span>2</span>
+                            </button>
+
+                            <button className="pagination__button">
+                                <span>3</span>
+                            </button>
+
+                            <button className="pagination__button">
+                                <span>4</span>
+                            </button>
+
+                            <button className="pagination__button">
+                                <span>5</span>
                             </button>
                         </div>
                     </div>
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,15 +1,14 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
 const Root = () => {
-    return (
+    return(
         <>
-            <Header />
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
+        <Header />
+        <Outlet />
+        <Footer />
         </>
     );
 }
